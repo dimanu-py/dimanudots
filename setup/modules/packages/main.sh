15e60a6c7b5128d2dbbe0c342253a2242a0f6e9a
dimanu-py <dimanu.py@gmail.com>
2026-01-19 08:20:30 +0100

    fix: source collect packages scripts in packages main file

--- setup/modules/packages/main.sh
+++ setup/modules/packages/main.sh
@@ -1,4 +1,5 @@
 source $DIMANUDOTS_SCRIPTS/modules/packages/args-parser.sh
 source $DIMANUDOTS_SCRIPTS/modules/packages/classify.sh
+source $DIMANUDOTS_SCRIPTS/modules/packages/collect-packages.sh
 source $DIMANUDOTS_SCRIPTS/modules/packages/install-packages.sh
 source $DIMANUDOTS_SCRIPTS/modules/packages/setup-yay.sh
