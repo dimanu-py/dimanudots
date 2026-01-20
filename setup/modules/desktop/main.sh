a997c7509faa66cd973edf647a146028a8e138a8
dimanu-py <dimanu.py@gmail.com>
2026-01-20 19:21:55 +0100

    feat: add snapshot management functionality with Timeshift integration

--- setup/modules/desktop/main.sh
+++ setup/modules/desktop/main.sh
@@ -2,3 +2,4 @@ source $DIMANUDOTS_SCRIPTS/modules/desktop/base-directories.sh
 source $DIMANUDOTS_SCRIPTS/modules/desktop/bluetooth.sh
 source $DIMANUDOTS_SCRIPTS/modules/desktop/network.sh
 source $DIMANUDOTS_SCRIPTS/modules/desktop/display-manager.sh
+source $DIMANUDOTS_SCRIPTS/modules/desktop/snapshots.sh
---
+++ setup/modules/desktop/snapshots.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+setup_snapshots_management() {
+  ensure_system_file_is_btrfs "/"
+  configure_timeshift
+  enable_snapshots_from_grub_menu
+}
+
+ensure_system_file_is_btrfs() {
+  local file_path="$1"
+
+  if file_system_is_not_btrfs $file_path; then
+      die "Error: The file '$file_path' is not on a Btrfs filesystem."
+  fi
+}
+
+file_system_is_not_btrfs() {
+  local file_path="$1"
+  local fs_type
+  fs_type=$(findmnt -n -o FSTYPE --target "$file_path")
+
+  [[ "$fs_type" != "btrfs" ]]
+}
+
+configure_timeshift() {
+  generate_config_file
+  configure_timeshift_schedule
+}
+
+generate_config_file() {
+  sudo timeshift --btrfs
+}
+
+configure_timeshift_schedule() {
+  local config_file="/etc/timeshift/timeshift.json"
+
+  ensure_file_exists "$config_file"
+
+  sudo sed -i \
+    -e 's/"schedule_monthly": *"[^"]*"/"schedule_monthly": "false"/' \
+    -e 's/"schedule_weekly": *"[^"]*"/"schedule_weekly": "false"/' \
+    -e 's/"schedule_daily": *"[^"]*"/"schedule_daily": "true"/' \
+    -e 's/"schedule_hourly": *"[^"]*"/"schedule_hourly": "false"/' \
+    -e 's/"schedule_boot": *"[^"]*"/"schedule_boot": "false"/' \
+    -e 's/"count_daily": *"[^"]*"/"count_daily": "3"/' \
+    "$config_file"
+}
+
+enable_snapshots_from_grub_menu() {
+  update_grub_menu
+  regenerate_grub_config
+  configure_grub_btrfsd_service_to_work_with_timeshift
+  enable_grub_btrfs_service
+}
+
+update_grub_menu() {
+  sudo /etc/grub.d/41_snapshots-btrfs
+}
+
+regenerate_grub_config() {
+  sudo grup-mkconfig -o /boot/grub/grub.cfg
+}
+
+configure_grub_btrfsd_service_to_work_with_timeshift() {
+  local service_file="/etc/systemd/system/grub-btrfsd.service"
+
+  ensure_file_exists "$service_file"
+
+  sudo sed -i 's|^ExecStart=.*|ExecStart=/usr/bin/grub-btrfsd --syslog --timeshift-auto|' "$service_file"
+}
+
+enable_grub_btrfs_service() {
+  enable_service "grub-btrfsd"
+}
