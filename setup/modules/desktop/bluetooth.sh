d217d9fa7069d371d32be23852ce69b722926b70
dimanu-py <dimanu.py@gmail.com>
2026-01-20 10:06:30 +0100

    feat: add Bluetooth service management script and integrate it into main script

---
+++ setup/modules/desktop/bluetooth.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+enable_bluetooth_service() {
+    enable_service "bluetooth.service"
+}
--- setup/modules/desktop/main.sh
+++ setup/modules/desktop/main.sh
@@ -1,1 +1,2 @@
 source $DIMANUDOTS_SCRIPTS/modules/desktop/base-directories.sh
+source $DIMANUDOTS_SCRIPTS/modules/desktop/bluetooth.sh
