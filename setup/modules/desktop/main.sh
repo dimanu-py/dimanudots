97cde778e4c196cc7038f166a9a7f1cb30bd54e1
dimanu-py <dimanu.py@gmail.com>
2026-01-20 19:29:46 +0100

    feat: add GTK theme setup script and update icon theme in configuration

--- setup/modules/desktop/main.sh
+++ setup/modules/desktop/main.sh
@@ -3,3 +3,4 @@ source $DIMANUDOTS_SCRIPTS/modules/desktop/bluetooth.sh
 source $DIMANUDOTS_SCRIPTS/modules/desktop/network.sh
 source $DIMANUDOTS_SCRIPTS/modules/desktop/display-manager.sh
 source $DIMANUDOTS_SCRIPTS/modules/desktop/snapshots.sh
+source $DIMANUDOTS_SCRIPTS/modules/desktop/gtk-themes.sh
