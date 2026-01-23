c313b4fa5718f7d646d9194d19bcda46bb30c609
dimanu-py <dimanu.py@gmail.com>
2026-01-23 07:50:33 +0100

    refactor: remove dotfiles module sourcing from main.sh

--- setup/modules/desktop/main.sh
+++ setup/modules/desktop/main.sh
@@ -3,6 +3,5 @@ source $DIMANUDOTS_SCRIPTS/modules/desktop/bluetooth.sh
 source $DIMANUDOTS_SCRIPTS/modules/desktop/network.sh
 source $DIMANUDOTS_SCRIPTS/modules/desktop/display-manager.sh
 source $DIMANUDOTS_SCRIPTS/modules/desktop/snapshots.sh
-source $DIMANUDOTS_SCRIPTS/modules/desktop/dotfiles.sh
 source $DIMANUDOTS_SCRIPTS/modules/desktop/themes.sh
 source $DIMANUDOTS_SCRIPTS/modules/desktop/wallpapers.sh
