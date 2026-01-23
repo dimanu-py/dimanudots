95ecaab59907abfa159b6a43f1c97c6e068d176d
dimanu-py <dimanu.py@gmail.com>
2026-01-23 16:47:48 +0100

    fix: remove set_owner and set_permissions commands for daemon.json in docker.sh

--- setup/modules/development/docker.sh
+++ setup/modules/development/docker.sh
@@ -25,8 +25,6 @@ _limit_docker_log_size() {
   }
 }
 EOF
-  set_owner "root" "$DOCKER_DAEMON_DIR/daemon.json" --sudo
-  set_permissions 0644 $DOCKER_DAEMON_DIR/daemon.json --sudo
 }
 
 _enable_docker_services() {
