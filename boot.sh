1893f6e5df4805a5d8ebd6b38263197b9949e87f
dimanu-py <dimanu.py@gmail.com>
2026-01-23 07:30:01 +0100

    feat: refactor boot.sh to streamline dotfiles installation process

--- boot.sh
+++ boot.sh
@@ -4,8 +4,12 @@ set -euo pipefail
 
 CLONE_DIR="$HOME/.local/share/dimanudots"
 
-_git_is_installed() {
-    command -v git >/dev/null 2>&1
+begin_dotfiles_installation() {
+    _install_git
+    _make_install_script_executable
+    _clone_dotfiles_repo
+    echo -e "\nInstallation of dimanudots starting..."
+    source "$CLONE_DIR/install.sh"
 }
 
 _install_git() {
@@ -15,13 +19,17 @@ _install_git() {
     sudo pacman -Syu --noconfirm --needed git
 }
 
-_repository_is_already_cloned() {
-    [[ -d "$CLONE_DIR/.git" ]]
+_git_is_installed() {
+    command -v git >/dev/null 2>&1
+}
+
+_make_install_script_executable() {
+    chmod +x ./install.sh
 }
 
 _clone_dotfiles_repo() {
     local repo_url="https://github.com/dimanu-py/dimanudots.git"
-    
+
     if _repository_is_already_cloned; then
         echo "dimanudots repository is already cloned in $CLONE_DIR, updating..."
         git -C "$CLONE_DIR" pull --rebase
@@ -30,11 +38,8 @@ _clone_dotfiles_repo() {
     git clone "$repo_url" "$CLONE_DIR"
 }
 
-_begin_dotfiles_installation() {
-    _install_git
-    _clone_dotfiles_repo
-    echo -e "\nInstallation of dimanudots starting..."
-    source "$CLONE_DIR/install.sh"
+_repository_is_already_cloned() {
+    [[ -d "$CLONE_DIR/.git" ]]
 }
 
-_begin_dotfiles_installation
+begin_dotfiles_installation
