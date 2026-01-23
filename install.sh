a4b45ae2cdfc0c28bf7fcc1a20703f10cfc648bc
dimanu-py <dimanu.py@gmail.com>
2026-01-23 07:27:04 +0100

    feat: implement step execution logic in install.sh for improved modularity

--- install.sh
+++ install.sh
@@ -13,11 +13,53 @@ source "$DIMANUDOTS_SCRIPTS/lib/main.sh"
 source "$DIMANUDOTS_SCRIPTS/modules/main.sh"
 source "$DIMANUDOTS_SCRIPTS/steps/main.sh"
 
-main() {
+run_steps() {
+  local all_steps=(
     step_10_packages
     step_15_dotfiles
     step_20_desktop
     step_30_development_tools
+  )
+  local steps_to_run=("$@")
+
+  if _no_steps_are_specified "${steps_to_run[@]}"; then
+    steps_to_run=("${all_steps[@]}")
+  fi
+
+  for step in "${steps_to_run[@]}"; do
+    _run_step "$step"
+  done
 }
 
-main
+_no_steps_are_specified() {
+  [[ $# -eq 0 ]]
+}
+
+_run_step() {
+  case "$step" in
+      packages)
+        step_10_packages
+        ;;
+      dotfiles)
+        step_15_dotfiles
+        ;;
+      desktop)
+        step_20_desktop
+        ;;
+      development-tools)
+        step_30_development_tools
+        ;;
+      -h|--help)
+        usage
+        return 0
+        ;;
+      *)
+        echo "Unknown step: $step"
+        usage
+        return 1
+        ;;
+  esac
+}
+
+
+run_steps
