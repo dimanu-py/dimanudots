19fb497acc098c8b466c27d117ce422f2e748eb2
dimanu-py <dimanu.py@gmail.com>
2026-01-25 21:56:13 +0100

    refactor: simplify configuration of sddm theme using tee

--- setup/modules/desktop/display-manager.sh
+++ setup/modules/desktop/display-manager.sh
@@ -14,45 +14,12 @@ _create_sddm_config_dir() {
 }
 
 _configure_sddm_theme() {
-  local themes=("Theme=sugar-candy", "CursorTheme=bibata-cursor-theme")
-
   create_file "$SDDM_THEME_CONFIG" --sudo
-  for line in "${themes[@]}"; do
-    _fill_sddm_theme_config "$line"
-  done
-}
-
-_fill_sddm_theme_config() {
-  local line="$1"
-  local key="${line%%=*}"
-
-  if _has_key "$SDDM_THEME_CONFIG" "$key"; then
-    _update_line_in_file "$SDDM_THEME_CONFIG" "$key" "$line"
-  else
-    _append_line_to_file "$SDDM_THEME_CONFIG" "$line"
-  fi
-}
-
-_has_key() {
-  local file_path="$1"
-  local key="$2"
-
-  grep -q "^${key}=" "$file_path"
-}
-
-_update_line_in_file() {
-  local file_path="$1"
-  local key="$2"
-  local new_line="$3"
-
-  sudo sed -i "s|^${key}=.*|${new_line}|" "$file_path"
-}
-
-_append_line_to_file() {
-  local file_path="$1"
-  local line="$2"
-
-  echo "$line" | sudo tee -a "$file_path" > /dev/null
+  sudo tee "$SDDM_THEME_CONFIG" > /dev/null << EOF
+[Theme]
+Current=sugar-candy
+CursorTheme=bibata-cursor-theme
+EOF
 }
 
 _enable_sddm_service() {
